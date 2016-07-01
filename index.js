8e4fbfdcc296dfe243d5ff1dd442124e68ed115b
fliptheweb <itisshortemailaddress@gmail.com>
2016-07-01 11:15:28 +0300

    Add rules for double semicolon pseudo elements

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
   "rules": {
     "color-no-invalid-hex": true,
-    "declaration-colon-space-after": "always",
     "max-empty-lines": 2,
     "indentation": 2,
-    "value-list-comma-space-before": "never",
+    "declaration-colon-space-after": "always",
     "declaration-block-semicolon-newline-after": "always",
+    "value-list-comma-space-before": "never",
     "block-closing-brace-newline-after": "always",
     "rule-nested-empty-line-before": ["always-multi-line", {
       "except": ["first-nested"],
@@ -13,6 +13,7 @@ module.exports = {
     }],
     "rule-non-nested-empty-line-before": ["always", {
       "ignore": ["after-comment"]
-    }]
+    }],
+    "selector-pseudo-element-colon-notation": "double"
   }
 }
